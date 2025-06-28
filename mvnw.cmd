b8b2d7224a7c83b5252404c02b65171aa118ab85
bhadri <emmadisettybhadri31@gmail.com>
2025-06-28 13:45:45 +0530

    i have update the mvnwcmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -176,7 +176,7 @@ set ERROR_CODE=1
 if not "%MAVEN_SKIP_RC%"=="" goto skipRcPost
 @REM check for post script, once with legacy .bat ending and once with .cmd ending
 if exist "%USERPROFILE%\mavenrc_post.bat" call "%USERPROFILE%\mavenrc_post.bat"
-if exist "%USERPROFILE%\mavenrc_post.cmd" call "%USERPROFILE%\mavenrc_post.cmd"
+if exist "%USERPROFILE%\mavenrc_post.cmd" call "%USERPROFILE%\mavenrc"
 :skipRcPost
 
 @REM pause the script if MAVEN_BATCH_PAUSE is set to 'on'
